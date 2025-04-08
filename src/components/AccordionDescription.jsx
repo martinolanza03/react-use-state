651b13618fc1fc80800c783cdcd1e5a9aa949fac
martinolanza03 <lanzamartino3@gmail.com>
2025-04-08 17:27:05 +0200

    feat: added check if there is no language

--- src/components/AccordionDescription.jsx
+++ src/components/AccordionDescription.jsx
@@ -1,4 +1,8 @@
 export default function AccordionDescription({ languages, actived }) {
+    if (actived === null) {
+        return <p>Nessun Linguaggio selezionato</p>
+    }
+
     return actived && <>
         <p>{languages[actived - 1].title}</p>
         <p>{languages[actived - 1].description}</p>
